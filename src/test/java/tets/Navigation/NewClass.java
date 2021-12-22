3fdd63e86bc2f22c85a698867640a4c3dc34b13d
mohamedJamalTestAutomationEngineer <engmjamal92@gmail.com>
2021-12-22 20:21:48 +0200

    this is from development brach before move to master

--- src/test/java/tets/Navigation/NewClass.java
+++ src/test/java/tets/Navigation/NewClass.java
@@ -6,5 +6,10 @@ public class NewClass {
 	public void printFromHere() {
 		System.out.println("this is to print methods");
 	}
+	
+	public void printFromHere2() {
+		System.out.println("this is to print methods 2");
+	}
+
 
 }
